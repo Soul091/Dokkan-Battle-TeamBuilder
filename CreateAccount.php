f90713972b551904b88af9b29573a3f4394363f2
Jophiel Nino Novoa <92817643+jophi22@users.noreply.github.com>
2021-12-07 01:15:11 -0800

    front end php

---
+++ CreateAccount.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CSE 111 Project</title>
+    </head>
+    
+    <body background = "background.png">
+        <h1>Create Account</h1>
+
+        <div clas = "container">
+
+            <form action = "HomePage.html" method = "post">
+
+                <div>
+                    <label for = "username"> User:  </label>
+                    <input type = "text" name ="username">
+                </div>
+
+                <div>
+                    <label for = "email"> Email: </label>
+                    <input type = "text" name ="email">
+                </div>
+
+                <div>
+                    <label for = "password"> Password:  </label>
+                    <input type = "text" name ="password">
+                </div>
+
+                <button type = "submit" name = "create"> Confirm </button>
+
+                <p>Already a user? <a href = "HomePage.html"><b> Login Here</b></a></p>
+                
+            </form>
+
+
+
+
+
+
+
+        </div>
+    </body>
+</html>
